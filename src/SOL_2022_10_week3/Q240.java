a6ebe4cb8a41f8fdd2ef82503ab533ea8227379d
dahee <daheewa128@gmail.com>
2022-10-24 20:19:18 +0900

    2022_10_week3 240. Search a 2D Matrix II

---
+++ src/SOL_2022_10_week3/Q240.java
@@ -0,0 +1,27 @@
+package SOL_2022_10_week3;
+
+public class Q240 {
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int row = matrix.length-1;
+        int col = 0;
+
+        while(row>=0 && col< matrix[row].length) {
+            if(matrix[row][col]>target) {
+               row--;
+            } else if(matrix[row][col]<target){
+                col++;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
+        int target = 20;
+
+        System.out.println(searchMatrix(matrix, target));
+
+    }
+}
