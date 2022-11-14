8c19322399629adb147698a168c17a2485ed655c
dahee <daheewa128@gmail.com>
2022-11-14 23:30:15 +0900

    2022_11_week2 7. Reverse Integer

--- src/SOL_2022_11_week2/Q7.java
+++ src/SOL_2022_11_week2/Q7.java
@@ -6,10 +6,6 @@ public class Q7 {
     public static int reverse(int x) {
         int ans = 0;
         int check = 0;
-        boolean isPositive = true;
-        if(x<0) isPositive = false;
-
-        x = Math.abs(x);
 
         while(x!=0) {
             check = check*10+x%10;
@@ -17,7 +13,6 @@ public class Q7 {
             x/=10;
             ans = check;
         }
-        if(!isPositive) ans*=-1;
 
         return ans;
     }
