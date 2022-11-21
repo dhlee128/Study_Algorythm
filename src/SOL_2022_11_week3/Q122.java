1559394beab9f96d075c342a56c13f55d1f7853a
dahee <daheewa128@gmail.com>
2022-11-21 21:53:10 +0900

    2022_11_week3 122. Best Time to Buy and Sell Stock II

---
+++ src/SOL_2022_11_week3/Q122.java
@@ -0,0 +1,19 @@
+package SOL_2022_11_week3;
+
+public class Q122 {
+
+    public static int maxProfit(int[] prices) {
+        int profit = 0;
+
+        for(int i=0; i<prices.length-1; i++) {
+            if(prices[i]<prices[i+1]) profit+=(prices[i+1]-prices[i]);
+        }
+
+        return profit;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,4};
+        System.out.println(maxProfit(prices));
+    }
+}
