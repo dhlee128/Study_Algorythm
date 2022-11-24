3d4eb0d3b7d15520aa6de5c3a03fe0a0ac9d8d84
dahee <daheewa128@gmail.com>
2022-11-24 22:10:32 +0900

    2022_11_week3 237. Delete Node in a Linked List

---
+++ src/SOL_2022_11_week4/Q237.java
@@ -0,0 +1,16 @@
+package SOL_2022_11_week4;
+
+public class Q237 {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+}
