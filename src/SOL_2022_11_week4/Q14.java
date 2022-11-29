79f1a9f236ff420b85074834ea5f2348e62deb37
dahee <daheewa128@gmail.com>
2022-11-29 19:59:44 +0900

    2022_11_week4 14. Longest Common Prefix

--- src/SOL_2022_11_week4/Q14.java
+++ src/SOL_2022_11_week4/Q14.java
@@ -3,12 +3,8 @@ package SOL_2022_11_week4;
 public class Q14 {
     public static String longestCommonPrefix(String[] strs) {
         String str = strs[0];
+        System.out.println("".indexOf(""));
 
-        for(int i=1;i<strs.length; i++) {
-            while(strs[i].indexOf(str)!=0) {
-                str = str.substring(0,str.length()-1);
-            }
-        }
         return str;
     }
 
