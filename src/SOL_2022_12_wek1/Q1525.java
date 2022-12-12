0c9c22fdbd98a701bbc07ce950946f21dbc78bb1
dahee <daheewa128@gmail.com>
2022-12-12 23:08:09 +0900

    2022_12_week1 1525. Number of Good Ways to Split a String

---
+++ src/SOL_2022_12_wek1/Q1525.java
@@ -0,0 +1,33 @@
+package SOL_2022_12_wek1;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Q1525 {
+    public static int numSplits(String s) {
+        int cnt = 0;
+        int[] pre = new int[s.length()];
+        int[] suf = new int[s.length()];
+
+        Set<Character> set = new HashSet<>();
+        for(int i=0; i<s.length(); i++) {
+            set.add(s.charAt(i));
+            pre[i] = set.size();
+        }
+        set = new HashSet<>();
+        for(int i=s.length()-1; i>=0; i--) {
+            set.add(s.charAt(i));
+            suf[i] = set.size();
+        }
+
+        for(int i=0; i<s.length()-1; i++) {
+            if(pre[i]==suf[i+1]) cnt++;
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        String s = "aacaba";
+        System.out.println(numSplits(s));
+    }
+}
